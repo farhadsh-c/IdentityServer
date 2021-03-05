ba9b470dceff9debec6b81bf27d359e6804750e6
farhad <farhad.shariat3@gmail.com>
2021-03-05 17:37:24 +0330

    First Commit

---
+++ ApiTwo/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace ApiTwo.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        [Route("/")]
+        public async Task<IActionResult>Index()
+        {
+            var serverClient = httpClientFactory.CreateClient();
+
+            var discoveryDocument = await serverClient.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+            {
+                Address= "https://localhost:44376/"
+            });
+            var tokenResponse = await serverClient.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = discoveryDocument.TokenEndpoint,
+                    ClientId="client_id",
+                    ClientSecret="client_secret",
+                    Scope="ApiOne"
+                }
+                ); 
+            var apiClient= httpClientFactory.CreateClient();
+
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:44318/secret");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+
+
+            return Ok(new
+            {
+                access_token = tokenResponse.AccessToken,
+                message=content
+            }) ; 
+        }
+    }
+}
---
+++ ApiTwo/Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiTwo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", config =>
+                {
+                    config.Authority = "https://localhost:44376/";
+
+                    config.Audience = "ApiTwo";
+                });
+            //be able to request the token and use token to call ApiOne
+            services.AddHttpClient();
+
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+           
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
